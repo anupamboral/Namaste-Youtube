cf5eb80847ba69bd71f7605bc1f7b7caaa66fddf
anupamboral <anupamboral6889@gmail.com>
2024-12-28 04:39:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App bg-slate-950">
+      <h1 className="text-red-700 text-6xl mt-10 text-center">hi</h1>
+    </div>
+  );
+}
+
+export default App;
